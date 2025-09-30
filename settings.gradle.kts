c15e96e6b0d418e9b7b4a8e3ae33050fb0e83335
kigawa <kigawa.8390@gmail.com>
2025-09-30 16:00:20 +0900

    Kotlinバージョンを2.1.10に更新

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "org.jetbrains.kotlin.jvm" || requested.id.id == "org.jetbrains.kotlin.plugin.spring") {
+                useVersion("2.1.10")
+            }
+        }
+    }
+}
+
 rootProject.name = "keruta-executor"
 
 plugins {
